0fdc2157f40bc5c7ab379c9ed5b17547099dac29
Lukatc <lukachalidze7878@gmail.com>
2026-07-13 17:49:34 +0400

    Update app.py
    Added a new route to serve the index.html file.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, jsonify, send_from_directory
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime, timedelta
@@ -634,3 +635,7 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
